60aba5ffa51bac14ae75c7bfb2d285e48ae6f28a
Eugene <eugene.dodin@gmail.com>
2019-12-25 11:03:19 +0200

    Implemented base main screen which loads first page of popular movies

--- app/src/main/java/com/dodin/doneittest/MainActivity.kt
+++ app/src/main/java/com/dodin/doneittest/MainActivity.kt
@@ -1,12 +1,56 @@
 package com.dodin.doneittest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dodin.doneittest.common.Lce
+import com.dodin.doneittest.databinding.ActivityMainBinding
+import com.dodin.doneittest.di.ViewModelFactory
+import com.dodin.doneittest.ui.MainViewModel
+import com.dodin.doneittest.ui.adapters.MoviesAdapter
+import com.google.android.material.snackbar.Snackbar
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var factory: ViewModelFactory
+    private lateinit var model: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        (application as App).appComponent.inject(this)
+        model = ViewModelProvider(this, factory).get(MainViewModel::class.java)
+
+        initList()
+        initObservers()
+    }
+
+    private fun initObservers() {
+        model.movies.observe(this, Observer {
+            binding.progress.isVisible = it is Lce.Loading
+            when(it) {
+                is Lce.Error -> {
+                    Snackbar.make(binding.root, it.message, Snackbar.LENGTH_SHORT).show()
+                }
+                is Lce.Success -> {
+                    (binding.list.adapter as MoviesAdapter).submitList(it.data)
+                }
+            }
+        })
+    }
+
+    private fun initList() {
+        with(binding.list) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = MoviesAdapter()
+        }
     }
 }
--- app/src/main/java/com/dodin/doneittest/ui/MainViewModel.kt
+++ app/src/main/java/com/dodin/doneittest/ui/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.dodin.doneittest.ui
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dodin.doneittest.common.Lce
 import com.dodin.doneittest.common.Schedulers
 import com.dodin.doneittest.data.remote.MovieApiService
 import com.dodin.doneittest.data.remote.dto.Movie
@@ -11,21 +12,22 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(private val api: MovieApiService) : ViewModel() {
     private val disposable = CompositeDisposable()
 
-    val movies = MutableLiveData<List<Movie>>()
+    val movies = MutableLiveData<Lce<List<Movie>>>()
+    private val moviesList: MutableList<Movie> = mutableListOf()
 
     init {
         disposable.add(
             api.getPopular()
                 .subscribeOn(Schedulers.network())
                 .observeOn(Schedulers.ui())
+                .doOnSubscribe { movies.postValue(Lce.Loading) }
                 .subscribe(
                     {
-                        val previous = movies.value ?: emptyList()
-                        val total = previous + it.results
-                        movies.postValue(total)
+                        moviesList.addAll(it.results)
+                        movies.postValue(Lce.Success(moviesList))
                     },
                     {
-
+                        movies.postValue(Lce.Error(it.localizedMessage ?: ""))
                     }
                 )
         )
