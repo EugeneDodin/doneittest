19d275578d39b7fce7506ac5187bb570c15935f6
Eugene <eugene.dodin@gmail.com>
2019-12-25 16:20:03 +0200

    Changed singleton to regular class

--- app/src/main/java/com/dodin/doneittest/common/Schedulers.kt
+++ app/src/main/java/com/dodin/doneittest/common/Schedulers.kt
@@ -3,7 +3,7 @@ package com.dodin.doneittest.common
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
-object Schedulers {
+class Schedulers {
     fun network() = Schedulers.io()
     fun ui() = AndroidSchedulers.mainThread()
 }
